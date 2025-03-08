d8b282076c4780d53c783a480f4d6f5ef1018796
ErikNas <maileriknas@gmail.com>
2025-03-08 12:41:59 +0300

    Создание проекта

---
+++ src/main/java/ru/eriknas/remind_me_bot/RemindMeBotApplication.java
@@ -0,0 +1,13 @@
+package ru.eriknas.remind_me_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindMeBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindMeBotApplication.class, args);
+	}
+
+}
